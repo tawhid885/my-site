bc3a33d890b8405f2638bcbde654a2cacaea10db
tawhid885 <hmdshakil885@gmail.com>
2023-07-23 23:19:40 +0600

    post card add

--- src/Pages/Home/Posts/Posts.js
+++ src/Pages/Home/Posts/Posts.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import React from 'react';
 import health_1 from "../../../images/health1.jpg";
 import "./Posts.css";
@@ -7,30 +7,42 @@ import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 
 const Posts = () => {
     return (
-        <Box>
+        <Box className="posts_padding">
             <Grid container spacing={3}>
-                <Grid item xs={12} md={8}>
+                {/* post image and rest of work  */}
+                <Grid item xs={12} md={8} className='posts_container'>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
+                        {/* image container  */}
+                        <Grid item xs={12} md={6} className='posts_image_container'>
                             <img src={health_1} className='posts_images'/>
                         </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Grid container>
-                                <Grid item xs={6} md={6}>
-                                    <AccessTimeIcon className='color'/>
+                        {/* post detail container  */}
+                        <Grid item xs={12} md={6} className='posts_detail_container'>
+                            {/* date and folder  */}
+                            <Grid container className='posts_date_container'>
+                                <Grid item xs={6} md={6} className='posts_clock'>
+                                    <AccessTimeIcon size="small" className='posts_clock_icon'/>
                                     <Typography variant='small' className='posts_date_sub'>October3, 20118</Typography>
                                 </Grid>
-                                <Grid item xs={6} md={6}>
-                                    <FolderOpenIcon className='color'/>
+                                <Grid item xs={6} md={6} className='posts_folder'>
+                                    <FolderOpenIcon size="small" className='posts_folder_icon'/>
                                     <Typography variant='small' className='posts_date_sub'>Coaching, Recipies</Typography>
                                 </Grid>
                             </Grid>
-                            <a href="" style={{textDecoration:"none", color:'black'}}>
-                            <Typography variant="h6"className='posts_heading'>The absolute best foods for getting that youthful glow</Typography>
+                            {/* post heading  */}
+                            <a href="" className='posts_heading_a'>
+                                <h3 className='posts_heading'>The absolute best foods for getting that youthful glow</h3>
+                            </a>
+                            {/* post detail  */}
+                            <p variant="normal" className='posts_detail'>Prepared do an dissuade be so whatever steepest. Yet her beyond 
+                            looked either day wished nay. By doubtful disposed do juvenile an.</p>
+                            <a href='' className='posts_readmore'>
+                                <p>Read more <span className='posts_arrow'> > </span></p>
                             </a>
                         </Grid>
                     </Grid>
                 </Grid>
+                {/* sidebar portion  */}
                 <Grid item xs={12} md={4}>
                     <h2>hossen</h2>
                 </Grid>
